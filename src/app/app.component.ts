9411a7221620e3e7f94733f7c7dabf03d46831c8
momopeace <[mohamedkadourparis@gmail.com*](mailto:mohamedkadourparis@gmail.com)>
2023-01-11 10:18:46 +0100

    sauvegarde

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'JohnDoe';
+  user = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg',
+    hide : true
+  };
+ 
+
+  myAge(){
+this.user.hide = !this.user.hide;
+  }
+}
